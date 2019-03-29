9c8d35248364692d66158cb3b76688fd0cc89e2b
EsFrost <33174986+kuroneko4@users.noreply.github.com>
2019-03-29 19:04:39 +0700

    Update EsFrost.sh

--- EsFrost.sh
+++ EsFrost.sh
@@ -9,6 +9,7 @@ echo "\033[32;1m ================================================"
 sleep 1
 echo " "
 echo "\033[36;1m Program By EsFrost Kuroneko4"
+sleep 1
 echo "\033[36;1m Author : Aldo Briansyah"
 echo "\033[36;1m Auto Run Python2
 echo " "
