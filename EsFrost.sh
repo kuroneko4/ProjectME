dfa70745296a43ab4d2e8fad096693489abf2b23
EsFrost <33174986+kuroneko4@users.noreply.github.com>
2019-03-29 19:36:19 +0700

    Update EsFrost.sh

--- EsFrost.sh
+++ EsFrost.sh
@@ -6,18 +6,19 @@ clear
 toilet -f slant --gay "EsFrost"
 sleep 1
 echo "\033[32;1m ================================================"
-sleep 1
-echo " "
+sleep 2
 echo "\033[36;1m Program By EsFrost Kuroneko4"
 echo "\033[36;1m Author : Aldo Briansyah"
 echo "\033[36;1m Auto Run Python2"
-echo " "
-sleep 1
+sleep 2
 echo "\033[32;1m ================================================"
 sleep 1
 echo "\033[33;1m1 Run Python2"
 echo "\033[33;1m2 Cek Color"
 echo "\033[33;1m3 Keluar dulu"
+sleep 1
+echo "\033[32;1m ================================================"
+echo "\033[31;1m Pilih Salah Satu aja Ngenez :"
 read esfrost
 
 if [ $esfrost = 1 ] || [ $esfrost = 1 ]
@@ -27,3 +28,20 @@ python2
 
 fi
 
+if [ $esfrost = 2 ] || [ $esfrost = 2 ]
+then
+clear
+toilet -f slant --gay "ZONK"
+sleep 3
+echo "Masih dalam pengembangan mblo :V"
+
+fi
+
+if [ $esfrost = 3 ] || [ $esfrost = 3 ]
+then
+clear
+echo "Thanks sudah menggunakan Tools Saya Mblo :)"
+sleep 1
+echo " sampai jumpa lagi . . . "
+
+fi
