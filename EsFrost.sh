f345a8bbf72967077d4666132687da8358d8a397
EsFrost <33174986+kuroneko4@users.noreply.github.com>
2019-03-29 18:39:56 +0700

    Create EsFrost.sh

---
+++ EsFrost.sh
@@ -0,0 +1,13 @@
+//coding asli by EsFrost Kuroneko4
+
+clear
+toilet -f slant --gay "EsFrost"
+sleep 1
+echo "\033[32;1m ================================================"
+sleep 1
+echo " "
+echo "\033[31;1m Program By EsFrost"
+echo " "
+sleep 1
+echo "\033[32;1m ================================================"
+sleep 1
